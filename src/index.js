bbad2fc303e2787cebe7a185e13614b21e10409d
dptorrez <paolatrz1998@gmail.com>
2024-12-16 17:45:38 -0600

    Integrated the API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 function generatePoem(event) {
     event.preventDefault()
+
+    let poemInstructions = document.querySelector("#poem-instructions");
+    let apiKey = "bd9741tbdfabbd3bc4c44f3o556466c0";
+    let prompt = `Users instructions: Generate a romance poem about ${poemInstructions.value}`;
+    let context = "You are a love poem expert who is passionate about writing short poems. Your task is to generate a short 4 line poem and separate each line with a <br />. Make sure to stay on topic and follow the users instructions.";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+axios.get(apiUrl).then(revealPoem);
     
+}
+
+let poemForm = document.querySelector("#poem-generator-form");
+poemForm.addEventListener("submit", generatePoem);
+
+
+
+function revealPoem(response) {
 
     new Typewriter('#poem', {
-        strings: "Love is like a river, <br/> A never ending stream. ",
+        strings: response.data.answer ,
         autoStart: true,
         delay: 10,
       });
 }
 
-let poemForm = document.querySelector("#poem-generator-form");
-poemForm.addEventListener("submit", generatePoem);
+
+
+
